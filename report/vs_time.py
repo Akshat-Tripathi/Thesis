f41dd187eefcb6e76b1972f5ae79ada0875ba90d
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2023-06-01 14:46:23 +0100

    Made vs_time more generic

--- report/vs_time.py
+++ report/vs_time.py
@@ -7,39 +7,92 @@ matplotlib.rcParams.update({
     'pgf.rcfonts': False,
 })
 
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
 
 WSL_PATH = "//wsl.localhost//Ubuntu-20.04//home//akshat//projects//thesis//ikura//"
-path = f"{WSL_PATH}//experiments//results//mu_bound"
 
-te = pd.read_csv(os.path.join(WSL_PATH, path, "all_tes.csv"))
-te = te.where(te["attacker?"] == False)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="summarise.py",
+        description="Summarises the results of an experiment "
+    )
 
-te = te.drop(["#id", "run"], axis=1)
-te["TE"] /= 100
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        help="Path to the experiment",
+        dest="path",
+        required=True,
+    )
 
-te = te.dropna()
+    parser.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        help="Name of the boxplot",
+        dest="name",
+        required=True
+    )
 
-grouped = te.groupby(["onramp", "tick"])
-means = grouped.aggregate("mean").unstack(0).to_numpy()
-stddevs = grouped.aggregate("std").unstack(0)
+    parser.add_argument(
+        "-v",
+        "--vars",
+        nargs="*",
+        help="Independent variables",
+        dest="vars",
+        required=False,
+        default=[]
+    )
 
-fig, ax = plt.subplots()
+    parser.add_argument(
+        "-x",
+        "--x_axis",
+        type=str,
+        help="Label to put on the x axis",
+        required=True
+    )
 
-colours = plt.cm.jet(np.linspace(0,1,16))
+    args = parser.parse_args()
 
-for i in range(16):
-    ax.plot(means[:, i], color=colours[i])
+    te = pd.read_csv(os.path.join(WSL_PATH, args.path, "all_tes.csv"))
+    te = te.where(te["attacker?"] == False)
 
-ax.text(145, 0.55, "Onramp Size")
+    te = te.drop(["#id", "run"], axis=1)
+    te["TE"] /= 100
 
-ax.legend(range(1, 17), loc=(1.04, 0))
-ax.set_xlabel("Tick")
-ax.set_ylabel("Average Trajectory Error (m)")
+    te = te.dropna()
 
+    grouped = te.groupby(args.vars + ["tick"])
+    means = grouped.aggregate("mean").unstack(0).to_numpy()
+    stddevs = grouped.aggregate("std").unstack(0)
 
-plt.savefig(os.path.join("graphs", f"mu_bound_vs_time.pdf"),  bbox_inches="tight")
+    fig, ax = plt.subplots()
+
+    colours = plt.cm.jet(np.linspace(0,1,means.shape[1]))
+
+    for i in range(means.shape[1]):
+        ax.plot(means[:, i], color=colours[i])
+
+    # Horrible code
+    if "sybil" in args.path:
+        legend_title_x = 146
+        legend_range = range(20)
+        legend_fontsize = 8
+    elif "mu_bound" in args.path:
+        legend_title_x = 145
+        legend_range = range(1, 17)
+        legend_fontsize = 10
+
+    ax.text(legend_title_x, 0.55, args.x_axis)
+
+    ax.legend(legend_range, loc=(1.04, 0), fontsize=legend_fontsize)
+    ax.set_xlabel("Tick")
+    ax.set_ylabel("Average Trajectory Error (m)")
+
+    plt.savefig(os.path.join("graphs", f"{args.name}.pdf"),  bbox_inches="tight")
 
